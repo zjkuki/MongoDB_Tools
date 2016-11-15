252def206447b8f4b5f94290966a019306aaf2d8
kuki <kuki@365yiding.com>
2016-11-15 23:49:23 +0800

    kuki's version

---
+++ Backup/MongoDB.WebIDE/Models/ShowDataModel.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Model;
+using System.Collections.Generic;
+
+namespace MongoDB.WebIDE.Models
+{
+    public class ShowDataModel
+    {
+        public uint ID { get; set; }
+
+        public uint TblID { get; set; }
+
+        public string Title { get; set; }
+
+        public List<MongoFieldModel> Fields { get; set; }
+    }
+}
